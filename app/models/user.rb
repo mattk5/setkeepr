e9a4cf194bf82d7cf6a400d6d351c122c2379d20
Matt <mattkubota5@gmail.com>
2018-06-27 17:00:28 -0400

    added bcrypt and validations to set up for use.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   before_save { email.downcase! }
   validates :name, presence: true, length: { maximum: 60 }
   validates :email, presence: true, length: { maximum: 200 }, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
+
+  has_secure_password
 end
